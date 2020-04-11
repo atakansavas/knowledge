7d8a061fdb03dd33c12d0daddf4181913707bf66
atakansavas <atakansavas@atakans-MacBook-Pro.local>
2020-04-11 22:53:36 +0300

    structre created. views created.

---
+++ src/views/layout/Layout.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+const { Content } = Layout;
+
+
+export default () => {
+
+    return (
+        <>
+            <Header />
+            <Content style={{ padding: '0 50px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
+                <div className="site-layout-content">Content</div>
+            </Content>
+            <Footer />
+        </>
+
+    );
+}
